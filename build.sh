15a8a33b2daa622e0b00f8d187886a0208819d6a
Ramona Pronestiù <pronestiramona@gmail.com>
2026-03-14 15:10:10 +0100

    chore: stash last edits

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-rsync -r ~/Local\ Sites/starsforeurope/app/public/wp-content/uploads/simply-static/temp-files/simply-static-*/ ./docs/
-git add .
-git commit -m 'update'
-git push
+WEBSITE_REPO=~/Desktop/website
